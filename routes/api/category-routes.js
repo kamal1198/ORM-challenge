956fdb40978faac220253fefeddbecccec7d6f1d
kamal1198 <kaur.preetkamal1198@gmail.com>
2022-06-17 13:24:56 -0400

    added category, product and tag by its id value

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,24 +5,29 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({include:[Product]}).then(categorydata => res.json(categorydata))
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findOne({where:{id:req.params.id},include:[Product]}).then(categorydata => res.json(categorydata))
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+Category.create(req.body).then(categorydata => res.json(categorydata))
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body,{where:{id:req.params.id}}).then(categorydata => res.json(categorydata))
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({where:{id:req.params.id}}).then(categorydata => res.json(categorydata))
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,12 +6,15 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({include:[Category,{model:Tag,through:ProductTag}]}).then(productdata => res.json(productdata))
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findAll({where:{id:req.params.id},include:[Category,{model:Tag,through:ProductTag}]}).then(productdata => res.json(productdata))
+
   // be sure to include its associated Category and Tag data
 });
 
@@ -91,6 +94,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({where:{id:req.params.id}}).then(productdata => res.json(productdata))
 });
 
 module.exports = router;
